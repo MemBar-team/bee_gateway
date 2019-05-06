7baa7c7ef42f5fa8b755008951642c846a400132
kukku723 <z03177279@gmail.com>
2019-05-06 12:54:15 +0900

    update

--- models/mysqlHandler.go
+++ models/mysqlHandler.go
@@ -2,13 +2,14 @@ package models
 
 import (
 	"fmt"
+
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 	"github.com/davecgh/go-spew/spew"
 )
 
-func init(){
-	dblink:=fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=%s",
+func init() {
+	dblink := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=%s",
 		beego.AppConfig.String("dbuser"),
 		beego.AppConfig.String("dbpassword"),
 		beego.AppConfig.String("dbhost"),
@@ -17,10 +18,10 @@ func init(){
 		beego.AppConfig.String("psqlssl"),
 	)
 	spew.Dump(dblink)
-	orm.RegisterDriver("postgres",orm.DRPostgres)
-	orm.RegisterDataBase(beego.AppConfig.String("dbalianame"),beego.AppConfig.String("dbdrivename"), dblink)
+	orm.RegisterDriver("postgres", orm.DRPostgres)
+	orm.RegisterDataBase(beego.AppConfig.String("dbalianame"), beego.AppConfig.String("dbdrivename"), dblink)
 	//orm.RegisterDataBase("default", "postgres", "user=postgres password=test dbname=test1 host=127.0.0.1 port=5432 sslmode=disable")
 	orm.RegisterModel(
-		new(Comment))
-	orm.RunSyncdb("default",false,true)
+		new(User))
+	orm.RunSyncdb("default", false, true)
 }
