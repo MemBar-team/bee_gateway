7af1c96b4d0b73a7e07abb214d7a1783a795eec4
kukku723 <z03177279@gmail.com>
2019-05-28 22:00:55 +0900

    update migrate

--- models/user/user.go
+++ models/user/user.go
@@ -1,22 +1,33 @@
 package user
 
 import (
+	"github.com/astaxie/beego"
+	"github.com/davecgh/go-spew/spew"
 	"time"
 
 	"github.com/bee_getway/models"
 )
 
 type User struct {
-	Id       string `orm:"pk;unique;" json:"id"`
+	Id       string `gorm:"pk;unique;" json:"id"`
 	UserType uint8  `json:"user_type"`
-	Email    string `orm:"unique;" json:"email"`
+	Email    string `gorm:"unique;" json:"email"`
 	Password string `json:"password"`
 	Modified time.Time
 	Create   time.Time
 }
 
 func init() {
-	//orm.RegisterModel(new(Users))
-	db := models.GormConnect()
-	db.AutoMigrate(&User{})
+	devmode, err := beego.AppConfig.Bool("devmode")
+	if err != nil {
+		panic(err.Error())
+	}
+	if devmode {
+		db := models.GormConnect()
+		if err != nil {
+			panic(err.Error())
+		}
+		spew.Dump("created table Users")
+		db.AutoMigrate(&User{})
+	}
 }
