d5bfd33a9ffae4063ce06c99f3e9d3adb803f3b6
kukku723 <z03177279@gmail.com>
2019-06-23 16:53:40 +0900

    update jwt認証

--- utils/jwt.go
+++ utils/jwt.go
@@ -1,29 +1,71 @@
 package utils
 
 import (
+	"errors"
 	"github.com/astaxie/beego"
 	"github.com/bee_gateway/models/entity"
 	"github.com/dgrijalva/jwt-go"
 	"time"
 )
 
+const (
+	UserID = "id"
+	UserEmail = "email"
+	UserType = "usertype"
+	UserIat = "iat"
+	Userexp = "exp"
+)
+
+var (
+	secretKey string
+	ErrAbsent  = "token absent"  // token存在しない
+	ErrInvalid = "token invalid" // token無効
+	ErrExpired = "token expired" // token機関超過
+	ErrOther   = "other error"   // token未知エラー
+)
+func init() {
+	secretKey = beego.AppConfig.String("secretKey")
+}
+
 func CreateJWT(user entity.User) string {
 
-	secretKey := beego.AppConfig.String("secretKey")
-
 	// headerのセット
-	token := jwt.New(jwt.SigningMethodHS256)
-
 	// claimsのセット
-	claims := token.Claims.(jwt.MapClaims)
-	claims["id"] = user.Id
-	claims["email"] = user.Email
-	claims["userType"] = user.UserType
-	claims["iat"] = time.Now()
-	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		UserID: user.Id,
+		UserEmail: user.Email,
+		UserType: user.UserType,
+		UserIat: time.Now(),
+		Userexp: time.Now().Add(time.Hour * 24).Unix(),
+	})
 
 	//電子署名
 	tokenString, _ := token.SignedString([]byte(secretKey))
 
 	return tokenString
+}
+
+func ParseByte(token *jwt.Token) (interface{}, error) {
+	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+		return "", errors.New(ErrOther)
+	}
+	return []byte(secretKey), nil
+}
+
+
+func ParseToken(stringToken string) (entity.User,error) {
+	if stringToken == "" {
+		return entity.User{}, errors.New(ErrAbsent)
+		}
+	token, err := jwt.Parse(stringToken, ParseByte)
+
+	if err != nil {
+		return entity.User{},errors.New(ErrOther)
+	}
+	if token == nil {
+		return entity.User{}, errors.New(ErrOther)
+	}
+
+	return entity.User{},nil
+
 }
